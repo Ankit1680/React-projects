24fb3fbe744048d0255cd6d049bb88d5276e9e43
React-projects <avishwakarma8855@gmail.com>
2024-03-09 14:53:03 +0530

    button added

--- home-versity/src/Profile.jsx
+++ home-versity/src/Profile.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Profile() {
   return (
     <div>
-      <h1>profile page has new branch called as feature1</h1>
+      <h1>profile page has new branch called as feature1 (success)</h1>
     </div>
   );
 }
